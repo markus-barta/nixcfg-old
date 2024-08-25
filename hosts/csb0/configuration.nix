a89890935c4b1d328a5caee12cdb63542b03375e
Markus Barta <markus@barta.com>
2024-08-25 18:32:09 +0200

    fix: remove unused zellij themes, fix indents and comments

--- hosts/csb0/configuration.nix
+++ hosts/csb0/configuration.nix
@@ -1,16 +1,16 @@
-# csb0 (legacy name: onlineserver01) Netcup server mba
+# Configuration for csb0 (legacy name: onlineserver01) Netcup server mba
 { modulesPath, config, pkgs, username, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      ../../modules/mixins/server-remote.nix
-      ../../modules/mixins/server-mba.nix
-      ./disk-config.zfs.nix
-    ];
+  # Import necessary configuration modules
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/mixins/server-remote.nix
+    ../../modules/mixins/server-mba.nix
+    ./disk-config.zfs.nix
+  ];
 
-  # Bootloader.
+  # Bootloader configuration
   boot.supportedFilesystems = [ "zfs" ];
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
   services.zfs.autoScrub.enable = true;
@@ -21,10 +21,11 @@
     efiSupport = true;
     efiInstallAsRemovable = true;
     mirroredBoots = [
-      { devices = [ "nodev"]; path = "/boot"; }
+      { devices = [ "nodev" ]; path = "/boot"; }
     ];
   };
 
+  # Network settings for the initial RAM disk (initrd)
   boot.initrd.network = {
     enable = true;
     postCommands = ''
@@ -33,18 +34,19 @@
     '';
   };
 
+  # Networking configuration
   networking = {
-    hostId = "dabfdc01";  # needed for ZFS
+    hostId = "dabfdc01";  # Needed for ZFS
     hostName = "csb0";
     networkmanager.enable = true;
 
-    # ssh is already enabled by the server-common mixin
+    # Firewall configuration
     firewall = {
-      # allowedTCPPorts = [ 25 ]; # SMTP
+      # allowedTCPPorts = [ 25 ]; # Uncomment to allow SMTP
     };
   };
 
-  ## mba <
+  # Mosquitto configuration mba <
   users.groups.mosquitto = {
     gid = 1883;
   };
@@ -55,94 +57,70 @@
     chown -R ${username}:mosquitto /home/${username}/docker/mosquitto
     chmod -R 775 /home/${username}/docker/mosquitto
   '';
-  ## > mba
+  # > mba
 
+  # Environment system packages
   environment.systemPackages = with pkgs; [
+    # Add system packages here
   ];
 
-  # Custom Zellij Keybinds
+  # Home Manager configuration for custom Zellij keybindings and themes
   home-manager.users.${username} = {
     home.file."/home/mba/.config/zellij/config.kdl".text = ''
 
+      # Zellij keybindings configuration
       keybinds {
-
         unbind "Ctrl o"
 
         normal {
-            bind "Ctrl e" { SwitchToMode "Session"; }
+          bind "Ctrl e" { SwitchToMode "Session"; }
         }
 
         session {
           bind "Ctrl e" { SwitchToMode "Normal"; }
         }
-
       }
 
+      # Zellij themes configuration
       themes {
 
-          cyber-noir {
-              bg "#0b0e1a" // Dark Blue
-              fg "#91f3e4" // Teal
-              red "#ff578d" // Hot Pink
-              green "#00ff00" // Neon Green
-              blue "#3377ff" // Electric Blue
-              yellow "#ffd700" // Cyber Yellow
-              magenta "#ff00ff" // Neon Purple
-              orange "#ff7f50" // Cyber Orange
-              cyan "#00e5e5" // Cyan
-              black "#000000" // Black
-              white "#91f3e4" // Teal
-          }
+        # csb0 theme
+        csb0 {
+          bg "#9999ff"  // + Text Selection
+          fg "#6666af"  // + Footer Buttons
+          red "#f0f0f0" // + Shortcuts in Buttons (best: white)
+          green "#9999ff" // + Frame
+          blue "#00d9e3" // Electric Blue
+          yellow "#aae600" // Neon Yellow
+          magenta "#aa00ff" // Neon Purple
+          orange "#006611" // Retro Red
+          cyan "#00e5e5" // Cyan
+          black "#00000f" // + Header and Footer bg (Black)
+          white "#ffffff" // White
+        }
 
-          blade-runner {
-              bg "#1a1a1a" // Dark Gray
-              fg "#2bbff4" // Neon Blue
-              red "#ff355e" // Neon Pink
-              green "#00ff00" // Neon Green
-              blue "#00d9e3" // Electric Blue
-              yellow "#ffe600" // Neon Yellow
-              magenta "#ff00ff" // Neon Purple
-              orange "#ff8c0d" // Cyber Orange
-              cyan "#00e5e5" // Cyan
-              black "#000000" // Black
-              white "#ffffff" // White
-          }
-
-          retro-wave {
-              bg "#1a1a1a" // Dark Gray
-              fg "#ff9900" // Retro Orange
-              red "#ff355e" // Neon Pink
-              green "#00ff00" // Neon Green
-              blue "#00d9e3" // Electric Blue
-              yellow "#ffe600" // Neon Yellow
-              magenta "#ff00ff" // Neon Purple
-              orange "#ff6611" // Retro Red
-              cyan "#00e5e5" // Cyan
-              black "#000000" // Black
-              white "#ffffff" // White
-          }
-
-          csb0 {
-              bg "#9999ff" // + Text Selection
-              fg "#6666af" // + Footer Buttons
-              red "#f0f0f0" // + Shortcuts in Buttons (best: white)
-              green "#9999ff" // + Frame
-              blue "#00d9e3" // Electric Blue
-              yellow "#aae600" // Neon Yellow
-              magenta "#aa00ff" // Neon Purple
-              orange "#006611" // Retro Red
-              cyan "#00e5e5" // Cyan
-              black "#00000f" // + Header and Footer bg (Black)
-              white "#ffffff" // White
-          }
+        # ms theme
+        ms {
+          bg "#9f9f9f"  // + Text Selection
+          fg "#6f6f6f"  // + Footer Buttons
+          red "#f0f0f0" // + Shortcuts in Buttons (best: white)
+          green "#9f9f9f" // + Frame
+          blue "#00d9e3" // Electric Blue
+          yellow "#aae600" // Neon Yellow
+          magenta "#aa00ff" // Neon Purple
+          orange "#006611" // Retro Red
+          cyan "#00e5e5" // Cyan
+          black "#00000f" // + Header and Footer bg (Black)
+          white "#ffffff" // White
+        }
       }
 
+      # Zellij session configuration
       session_serialization true;
+
+      # Theme to be used by Zellij
       theme "csb0";
 
-      '';
+    '';
   };
-
-
-
 }
