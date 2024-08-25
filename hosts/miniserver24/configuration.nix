fd1079c38c13e361471229697db8d15ca29f077f
Markus Barta <markus@barta.com>
2024-08-25 19:02:16 +0200

    fix: zellij config with fixed keybind and new themes

--- hosts/miniserver24/configuration.nix
+++ hosts/miniserver24/configuration.nix
@@ -1,54 +1,53 @@
-  # miniserver24 server for mba
+# miniserver24 server for mba
 { modulesPath, config, pkgs, username, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      ../../modules/mixins/server-local.nix
-      ../../modules/mixins/server-mba.nix
-      ./disk-config.zfs.nix
-    ];
+  # Import necessary configuration modules
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/mixins/server-local.nix
+    ../../modules/mixins/server-mba.nix
+    ./disk-config.zfs.nix
+  ];
 
-  # Bootloader.
-  boot.supportedFilesystems = [ "zfs" ];
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  # Bootloader configuration
+  boot = {
+    supportedFilesystems = [ "zfs" ];
+    kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    loader.grub = {
+      enable = true;
+      zfsSupport = true;
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      mirroredBoots = [
+        { devices = [ "nodev" ]; path = "/boot"; }
+      ];
+    };
+    initrd.network = {
+      enable = true;
+      postCommands = ''
+        sleep 2
+        zpool import -a;
+      '';
+    };
+  };
+
+  # ZFS configuration
   services.zfs.autoScrub.enable = true;
 
-  boot.loader.grub = {
-    enable = true;
-    zfsSupport = true;
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-    mirroredBoots = [
-      { devices = [ "nodev"]; path = "/boot"; }
-    ];
-  };
-
-  boot.initrd.network = {
-    enable = true;
-    postCommands = ''
-      sleep 2
-      zpool import -a;
-    '';
-  };
-
+  # Networking configuration
   networking = {
-    hostId = "dabfdb01";  # needed for ZFS
+    hostId = "dabfdb01";  # Needed for ZFS
     hostName = "miniserver24";
     networkmanager.enable = true;
-
     nameservers = ["192.168.1.100"];
     defaultGateway = "192.168.1.5";
-
     interfaces.enp3s0f0 = {
       ipv4.addresses = [{ address = "192.168.1.101"; prefixLength = 24; }];
     };
-
-    # SSH is already enabled by the server-common mixin
+    # Firewall configuration
     firewall = {
-      # Disable the firewall if not needed
-      enable = false;
+      enable = false;  # Firewall is disabled
       allowedTCPPorts = [
         80    # HTTP
         443   # HTTPS
@@ -66,8 +65,7 @@
     };
   };
 
-
-  # Turn off fail2ban, because firewall is turned off
+  # Disable fail2ban since firewall is turned off
   services.fail2ban.enable = false;
 
   # Increase ulimit for influxdb
@@ -80,27 +78,128 @@
 
   # Enable Fwupd
   # https://nixos.wiki/wiki/Fwupd
-  services.fwupd.enable = true;
+ services.fwupd.enable = true;
 
+  # Additional system packages
   environment.systemPackages = with pkgs; [
-    samba  # Needed for net command to remotely shut down the windows pc from node red and finall via homekit voice command
-    wol    # Needed for wake on lan of the windows 10 pc in node red - for a homekit voice command
+    samba  # Needed for net command to remotely shut down the Windows PC from Node-RED and finally via HomeKit voice command
+    wol    # Needed for wake-on-LAN of the Windows 10 PC in Node-RED - for a HomeKit voice command
   ];
 
-  # Custom Zellij Keybinds
+  # Home Manager configuration for custom Zellij keybindings and themes
   home-manager.users.${username} = {
     home.file."/home/mba/.config/zellij/config.kdl".text = ''
+
+      /***********************************************
+       *    WARNING: CRITICAL INDENTATION SECTION    *
+       * KDL config below MUST be properly indented. *
+       ***********************************************/
+
+      // Zellij keybindings configuration
       keybinds {
-        unbind "Ctrl o"
-        normal {
-            bind "Ctrl e" { SwitchToMode "Session"; }
-        }
-        session {
-          bind "Ctrl e" { SwitchToMode "Normal"; }
-        }
+          unbind "Ctrl o"
+          normal {
+              bind "Ctrl e" { SwitchToMode "Session"; }
+          }
+          session {
+              bind "Ctrl e" { SwitchToMode "Normal"; }
+          }
       }
-      session_serialization false;
+
+      // Zellij themes configuration
+      themes {
+          catppuccin-latte {
+              bg "#acb0be" // Surface2
+              fg "#acb0be" // Surface2
+              red "#d20f39"
+              green "#40a02b"
+              blue "#1e66f5"
+              yellow "#df8e1d"
+              magenta "#ea76cb" // Pink
+              orange "#fe640b" // Peach
+              cyan "#04a5e5" // Sky
+              black "#dce0e8" // Crust
+              white "#4c4f69" // Text
+          }
+
+          catppuccin-frappe {
+              bg "#626880" // Surface2
+              fg "#c6d0f5"
+              red "#e78284"
+              green "#a6d189"
+              blue "#8caaee"
+              yellow "#e5c890"
+              magenta "#f4b8e4" // Pink
+              orange "#ef9f76" // Peach
+              cyan "#99d1db" // Sky
+              black "#292c3c" // Mantle
+              white "#c6d0f5"
+          }
+
+          catppuccin-macchiato {
+              bg "#5b6078" // Surface2
+              fg "#cad3f5"
+              red "#ed8796"
+              green "#a6da95"
+              blue "#8aadf4"
+              yellow "#eed49f"
+              magenta "#f5bde6" // Pink
+              orange "#f5a97f" // Peach
+              cyan "#91d7e3" // Sky
+              black "#1e2030" // Mantle
+              white "#cad3f5"
+          }
+
+          catppuccin-mocha {
+              bg "#585b70" // Surface2
+              fg "#cdd6f4"
+              red "#f38ba8"
+              green "#a6e3a1"
+              blue "#89b4fa"
+              yellow "#f9e2af"
+              magenta "#f5c2e7" // Pink
+              orange "#fab387" // Peach
+              cyan "#89dceb" // Sky
+              black "#181825" // Mantle
+              white "#cdd6f4"
+          }
+
+          // csb0 theme
+          csb0 {
+              bg "#9999ff"  // + Text Selection
+              fg "#6666af"  // + Footer Buttons
+              red "#f0f0f0" // + Shortcuts in Buttons (best: white)
+              green "#9999ff" // + Frame
+              blue "#00d9e3" // Electric Blue
+              yellow "#aae600" // Neon Yellow
+              magenta "#aa00ff" // Neon Purple
+              orange "#006611" // Retro Red
+              cyan "#00e5e5" // Cyan
+              black "#00000f" // + Header and Footer bg (Black)
+              white "#ffffff" // White
+          }
+
+          // ms theme
+          ms {
+              bg "#9f9f9f"  // + Text Selection
+              fg "#6f6f6f"  // + Footer Buttons
+              red "#f0f0f0" // + Shortcuts in Buttons (best: white)
+              green "#9f9f9f" // + Frame
+              blue "#00d9e3" // Electric Blue
+              yellow "#aae600" // Neon Yellow
+              magenta "#aa00ff" // Neon Purple
+              orange "#006611" // Retro Red
+              cyan "#00e5e5" // Cyan
+              black "#00000f" // + Header and Footer bg (Black)
+              white "#ffffff" // White
+          }
+      }
+
+      // Zellij session configuration
+      session_serialization true
+
+      // Theme to be used by Zellij
+      theme "catppuccin-mocha"
     '';
   };
-
 }
