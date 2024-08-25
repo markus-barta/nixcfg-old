a0edd3e8dd80e21d50fa880de519da40f0879c2c
Markus Barta <markus@barta.com>
2024-08-25 19:05:14 +0200

    fix: zellij themes and keybinds

--- hosts/csb0/configuration.nix
+++ hosts/csb0/configuration.nix
@@ -68,58 +68,116 @@
   home-manager.users.${username} = {
     home.file."/home/mba/.config/zellij/config.kdl".text = ''
 
-      # Zellij keybindings configuration
+      /***********************************************
+       *    WARNING: CRITICAL INDENTATION SECTION    *
+       * KDL config below MUST be properly indented. *
+       ***********************************************/
+
+      // Zellij keybindings configuration
       keybinds {
-        unbind "Ctrl o"
-
-        normal {
-          bind "Ctrl e" { SwitchToMode "Session"; }
-        }
-
-        session {
-          bind "Ctrl e" { SwitchToMode "Normal"; }
-        }
+          unbind "Ctrl o"
+          normal {
+              bind "Ctrl e" { SwitchToMode "Session"; }
+          }
+          session {
+              bind "Ctrl e" { SwitchToMode "Normal"; }
+          }
       }
 
-      # Zellij themes configuration
+      // Zellij themes configuration
       themes {
+          catppuccin-latte {
+              bg "#acb0be" // Surface2
+              fg "#acb0be" // Surface2
+              red "#d20f39"
+              green "#40a02b"
+              blue "#1e66f5"
+              yellow "#df8e1d"
+              magenta "#ea76cb" // Pink
+              orange "#fe640b" // Peach
+              cyan "#04a5e5" // Sky
+              black "#dce0e8" // Crust
+              white "#4c4f69" // Text
+          }
 
-        # csb0 theme
-        csb0 {
-          bg "#9999ff"  // + Text Selection
-          fg "#6666af"  // + Footer Buttons
-          red "#f0f0f0" // + Shortcuts in Buttons (best: white)
-          green "#9999ff" // + Frame
-          blue "#00d9e3" // Electric Blue
-          yellow "#aae600" // Neon Yellow
-          magenta "#aa00ff" // Neon Purple
-          orange "#006611" // Retro Red
-          cyan "#00e5e5" // Cyan
-          black "#00000f" // + Header and Footer bg (Black)
-          white "#ffffff" // White
-        }
+          catppuccin-frappe {
+              bg "#626880" // Surface2
+              fg "#c6d0f5"
+              red "#e78284"
+              green "#a6d189"
+              blue "#8caaee"
+              yellow "#e5c890"
+              magenta "#f4b8e4" // Pink
+              orange "#ef9f76" // Peach
+              cyan "#99d1db" // Sky
+              black "#292c3c" // Mantle
+              white "#c6d0f5"
+          }
 
-        # ms theme
-        ms {
-          bg "#9f9f9f"  // + Text Selection
-          fg "#6f6f6f"  // + Footer Buttons
-          red "#f0f0f0" // + Shortcuts in Buttons (best: white)
-          green "#9f9f9f" // + Frame
-          blue "#00d9e3" // Electric Blue
-          yellow "#aae600" // Neon Yellow
-          magenta "#aa00ff" // Neon Purple
-          orange "#006611" // Retro Red
-          cyan "#00e5e5" // Cyan
-          black "#00000f" // + Header and Footer bg (Black)
-          white "#ffffff" // White
-        }
+          catppuccin-macchiato {
+              bg "#5b6078" // Surface2
+              fg "#cad3f5"
+              red "#ed8796"
+              green "#a6da95"
+              blue "#8aadf4"
+              yellow "#eed49f"
+              magenta "#f5bde6" // Pink
+              orange "#f5a97f" // Peach
+              cyan "#91d7e3" // Sky
+              black "#1e2030" // Mantle
+              white "#cad3f5"
+          }
+
+          catppuccin-mocha {
+              bg "#585b70" // Surface2
+              fg "#cdd6f4"
+              red "#f38ba8"
+              green "#a6e3a1"
+              blue "#89b4fa"
+              yellow "#f9e2af"
+              magenta "#f5c2e7" // Pink
+              orange "#fab387" // Peach
+              cyan "#89dceb" // Sky
+              black "#181825" // Mantle
+              white "#cdd6f4"
+          }
+
+          // csb0 theme
+          csb0 {
+              bg "#9999ff"  // + Text Selection
+              fg "#6666af"  // + Footer Buttons
+              red "#f0f0f0" // + Shortcuts in Buttons (best: white)
+              green "#9999ff" // + Frame
+              blue "#00d9e3" // Electric Blue
+              yellow "#aae600" // Neon Yellow
+              magenta "#aa00ff" // Neon Purple
+              orange "#006611" // Retro Red
+              cyan "#00e5e5" // Cyan
+              black "#00000f" // + Header and Footer bg (Black)
+              white "#ffffff" // White
+          }
+
+          // ms theme
+          ms {
+              bg "#9f9f9f"  // + Text Selection
+              fg "#6f6f6f"  // + Footer Buttons
+              red "#f0f0f0" // + Shortcuts in Buttons (best: white)
+              green "#9f9f9f" // + Frame
+              blue "#00d9e3" // Electric Blue
+              yellow "#aae600" // Neon Yellow
+              magenta "#aa00ff" // Neon Purple
+              orange "#006611" // Retro Red
+              cyan "#00e5e5" // Cyan
+              black "#00000f" // + Header and Footer bg (Black)
+              white "#ffffff" // White
+          }
       }
 
-      # Zellij session configuration
-      session_serialization true;
+      // Zellij session configuration
+      session_serialization true
 
-      # Theme to be used by Zellij
-      theme "csb0";
+      // Theme to be used by Zellij
+      theme "csb0"
 
     '';
   };
