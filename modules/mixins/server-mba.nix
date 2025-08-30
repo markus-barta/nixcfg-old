a3de2bddaa12ea948572720346a617bda6a4e371
Markus Barta <markus@barta.com>
2025-08-30 16:08:31 +0200

    server-mba: add github deployment creds

--- modules/mixins/server-mba.nix
+++ modules/mixins/server-mba.nix
@@ -14,6 +14,8 @@
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGIQIkx1H1iVXWYKnHkxQsS7tGsZq3SoHxlVccd+kroMC/DhC4MWwVnJInWwDpo/bz7LiLuh+1Bmq04PswD78EiHVVQ+O7Ckk32heWrywD2vufihukhKRTy5zl6uodb5+oa8PBholTnw09d3M0gbsVKfLEi4NDlgPJiiQsIU00ct/y42nI0s1wXhYn/Oudfqh0yRfGvv2DZowN+XGkxQQ5LSCBYYabBK/W9imvqrxizttw02h2/u3knXcsUpOEhcWJYHHn/0mw33tl6a093bT2IfFPFb3LE2KxUjVqwIYz8jou8cb0F/1+QJVKtqOVLMvDBMqyXAhCkvwtEz13KEyt"
       # miniserver24: node-red container ssh calls
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAhUleyXsqtdA4LC17BshpLAw0X1vMLNKp+lOLpf2bw1 mba@miniserver24"
+      # github
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHg1Y7vbAJs4dB7I0zez5nhz9/zYI6qaEt1+HefM/7qF github-actions-deployment@pixoo-daemon"
     ];
   };
 
@@ -55,4 +57,10 @@
   };
 
   environment.variables.EDITOR = "nano";
+
+  # GHCR (GitHub Container Registry) credentials for Docker
+  environment.sessionVariables = {
+    GHCR_USERNAME = "markus-barta";
+    # GHCR_PASSWORD → manual login
+  };
 }
